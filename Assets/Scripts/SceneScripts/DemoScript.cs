2c5aff9406cba72aee830e7c4f00dc72f1bcb5af
SuperValou <valentin.esmieu@tuta.io>
2021-05-20 23:30:41 +0200

    Add basic files

---
+++ Assets/Scripts/SceneScripts/DemoScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.SceneScripts
+{
+    public class DemoScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/Typewriters/TypewriterAnimator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Typewriters
+{
+    public class TypewriterAnimator : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
